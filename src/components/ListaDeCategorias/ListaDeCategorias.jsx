21da30c608eaa13c0bd4f924e6b6fa4b9caed63b
umparadoxo <legiaored@gmail.com>
2021-05-21 23:09:47 -0300

    Categoria
    Adicionados os primeiros arquivos para criação de categorias

---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+class ListaDeCategorias extends Component {
+    _handleEventoInput(e) {
+        if (e.key == 'Enter') {
+            let valorCategoria = e.target.value;
+            this.props.adicionarCategoria(valorCategoria);
+        }
+
+    }
+
+    render() {
+        return (
+            <section className='lista-categorias'>
+                <ul className='lista-categorias_lista'>
+                    {this.props.categorias.map((categoria, index) => {
+                        return <li key={index} className='lista-categorias_item'>{categoria}</li>
+                    }
+                    )
+                    }
+                </ul>
+                <input
+                    type='text'
+                    className='lista-categorias_input'
+                    placeholder='Adicionar nova categoria'
+                    onKeyUp={this._handleEventoInput.bind(this)}
+                />
+            </section>
+        );
+    }
+}
+
+export default ListaDeCategorias;
