93acc522974d3901ae5c6a878076e5b7cfdd7faa
umparadoxo <legiaored@gmail.com>
2021-05-21 23:12:36 -0300

    Update Categorias
    Corrigidos bugs

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class ListaDeCategorias extends Component {
     _handleEventoInput(e) {
-        if (e.key == 'Enter') {
+        if (e.key === 'Enter') {
             let valorCategoria = e.target.value;
             this.props.adicionarCategoria(valorCategoria);
         }
